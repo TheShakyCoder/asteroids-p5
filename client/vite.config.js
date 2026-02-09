700686d42da4cf12bd6ba233e4e12697127c1b1d
Sharif Khan <>
2026-02-09 19:44:59 +0000

    build deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,6 +8,8 @@ const origin = `${process.env.DDEV_PRIMARY_URL}:${port}`
 export default defineConfig({
   plugins: [vue()],
   build: {
+    outDir: '../public',
+    emptyOutDir: true,
     rollupOptions: {
       output: {
         manualChunks: {
